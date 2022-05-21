9fd041587ca99fc6764ba477118e04086db757f2
Tuba Asif <68794712+tuba-theProgrammer@users.noreply.github.com>
2022-05-21 14:13:24 +0500

    signIn-SignUp-pages

--- app.js
+++ app.js
@@ -1,41 +1,33 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express")
+const app=express()
+const path = require('path')
+var mongoose = require('mongoose')
+var bodyParser= require('body-parser')
+app.set(express.static(path.join(__dirname,"/public")))
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.set("view engine","ejs")
 
-var app = express();
+const mongoDb='mongodb+srv://tubaAsif:shabana1234@cluster0.soo8g.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(mongoDb, /*We place this to remove warning*/{ useNewUrlParser:
+  true, useUnifiedTopology: true }).then(()=>{
+  console.log("Connected to MongoDB database")
+  }).catch((e)=>{console.log(e.message)})
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.get('/',(req,res)=>{
+  res.render("signIn.ejs")
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/signUp',(req,res)=>{
+  res.render("signUp.ejs")
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('*',(req,res)=>{
+  res.status(404).json({msg:"Not found"})
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(5000,()=>{
+  console.log('hello mai ab phir ap hi ko sun rha hn')
+})
