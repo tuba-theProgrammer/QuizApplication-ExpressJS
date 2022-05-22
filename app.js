e95d46ba40faa75903c45976eba4eded96e86c54
Tuba Asif <68794712+tuba-theProgrammer@users.noreply.github.com>
2022-05-22 22:17:47 +0500

    setting-up-passportMiddleware

--- app.js
+++ app.js
@@ -9,18 +9,60 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.set("view engine","ejs")
 
+const passport = require('passport')
+const localStrategy = require('passport-local')
+const passportLocalMongoose= require('passport-local-mongoose')
+const user= require('./models/Users')
+
+app.use(require("express-session")({
+  secret:"Any normal Word",       //decode or encode session
+  resave: false,          
+  saveUninitialized:false    
+}));
+
+
+passport.serializeUser(User.serializeUser());       //session encoding
+passport.deserializeUser(User.deserializeUser());   //session decoding
+passport.use(new LocalStrategy(User.authenticate()));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 const mongoDb='mongodb+srv://tubaAsif:shabana1234@cluster0.soo8g.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(mongoDb, /*We place this to remove warning*/{ useNewUrlParser:
   true, useUnifiedTopology: true }).then(()=>{
   console.log("Connected to MongoDB database")
   }).catch((e)=>{console.log(e.message)})
 
+
+
+  // showing Homepage
 app.get('/',(req,res)=>{
-  res.render("signIn.ejs")
+  res.render("homePage.ejs",{
+    title:'HomePage',
+   })
+
 })
 
+// showing signup page
 app.get('/signUp',(req,res)=>{
-  res.render("signUp.ejs")
+  res.render("signUp.ejs",{
+    title:'Registration Page',
+  })
+})
+
+
+// showing Login page
+app.get('/login',(req,res)=>{
+  res.render("signIn.ejs",{
+    title:'Login Page',
+   })
+
+})
+
+// handling user logout
+app.get('/logout',(req,res)=>{
+  console.log('logging out')
 })
 
 app.use('*',(req,res)=>{
--- models/Users.js
+++ models/Users.js
@@ -1,2 +1,10 @@
 const mongoose= require("mongoose")
-
+const passportLocalMongoose = require('passport-local-mongoose')
+var userSchema= new mongoose.Schema({
+    name:String,
+    password:String,
+    email:String
+})
+userSchema.plugin(passportLocalMongoose)
+var userModel = mongoose.Model('user',userSchema)
+module.exports=  userModel
